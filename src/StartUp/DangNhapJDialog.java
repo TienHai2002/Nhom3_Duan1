e6f9a209e5bdfced6025b75cfa415921aa6350c5
NguyenCongTruongPH17481 <truongncph17481@fpt.edu.vn>
2021-11-24 00:41:50 +0700

    truongncph17481_SuaLaiDuongDanFile

--- src/StartUp/DangNhapJDialog.java
+++ src/StartUp/DangNhapJDialog.java
@@ -17,7 +17,7 @@ import javax.swing.JTextField;
 public class DangNhapJDialog extends javax.swing.JDialog {
     public Preferences pref = Preferences.userRoot().node("Remember");
     ImageIcon poster = new ImageIcon("src/icon/poster.gif");
-    ImageIcon logo = new ImageIcon("src/icon/Logo startup.png");
+    ImageIcon logo = new ImageIcon("src/icon/logo_2.png");
     ImageIcon show = new ImageIcon("src/icon/show.png");
     ImageIcon unshow = new ImageIcon("src/icon/unshow.png");
     NhanVienDAO dao = new NhanVienDAO();
@@ -112,6 +112,8 @@ public class DangNhapJDialog extends javax.swing.JDialog {
         pn1.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
 
         pn2.setBackground(new java.awt.Color(255, 255, 255));
+
+        lblIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icon/logo_2.png"))); // NOI18N
 
         Remember.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         Remember.setText("Nhớ mật khẩu");
